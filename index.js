378edda9ff9aa57fc6e69acccf0a8ac7f1076385
Haisen5217 <tanny1234tanny@gmail.com>
2026-08-23 19:15:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 
 const config = {
-  channelAccessToken: 'qmMG0xac02VHdKZvXc1KYR9YR7uN8fydOxAjTJiKsP8rgqMVc5n4iJIULUo5dXK+dE9tcES9d+wnshyPg9hKPzJJZkm60GQ/SqTKn//i02U/ziZUPoWLUqas5oKKtwxTgRrcr+iCW6nGXwBZEiFIeAdB04t89/1O/w1cDnyilFU=',
-  channelSecret: 'fc0e421ee81773c0a0a062ebbebcc6dd'
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
 };
 
 const client = new line.messagingApi.MessagingApiClient({
@@ -37,6 +37,6 @@ function handleEvent(event) {
   });
 }
 
-app.listen(3000, () => {
-  console.log('伺服器已啟動，port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('伺服器已啟動');
 });
